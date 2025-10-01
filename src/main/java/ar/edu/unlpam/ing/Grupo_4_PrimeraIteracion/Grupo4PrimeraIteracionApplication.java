cc7d71e9e6f156ce0ec388d461a1927ae5b71551
TomasHoffer2002 <tomihoffer2002@gmail.com>
2025-10-01 09:52:07 -0300

    main

---
+++ src/main/java/ar/edu/unlpam/ing/Grupo_4_PrimeraIteracion/Grupo4PrimeraIteracionApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.Grupo_4_PrimeraIteracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo4PrimeraIteracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo4PrimeraIteracionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlpam/ing/Grupo_4_PrimeraIteracion/Grupo4PrimeraIteracionApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.Grupo_4_PrimeraIteracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo4PrimeraIteracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
